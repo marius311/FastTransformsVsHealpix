a8dc1b834e6b815b776d93f547228d31ba0fa2a8
marius <mariusmillea@gmail.com>
2020-05-26 00:22:39 -0700

    minor fix

--- runbenchmarks.jl
+++ runbenchmarks.jl
@@ -24,8 +24,8 @@ timings = map([128,256,512,1024,2048]) do healpix_nside
     ft_backward = @belapsed $lm2k \ ($x2k * $(randn(T, nθ, nφ))) 
 
     # run Healpix
-    hpx_forward = @belapsed map2alm($(rand(12*healpix_nside^2)), ℓmax=4*healpix_nside)
-    hpx_backward = @belapsed alm2map!(Array{T}(undef,12*healpix_nside^2), $(randn(Complex{T}, 1, 4*healpix_nside+1, 4*healpix_nside+1)))
+    hpx_forward = @belapsed map2alm($(rand(12*healpix_nside^2)), ℓmax=4*$healpix_nside)
+    hpx_backward = @belapsed alm2map!(Array{T}(undef,12*$healpix_nside^2), $(randn(Complex{T}, 1, 4*healpix_nside+1, 4*healpix_nside+1)))
 
     [healpix_nside, ft_forward, hpx_forward, ft_backward, hpx_backward]
 end
@@ -33,5 +33,5 @@ end
 pretty_table(
     hcat(timings...)',
     ["Nside" "FT Forward" "Hpx Forward" "FT Backward" "Hpx Backward";
-     ""      "[s]"        "[s]"         "[s]"         "[s]"],
+     ""      "[seconds]"  "[seconds]"   "[seconds]"   "[seconds]"]
 )
